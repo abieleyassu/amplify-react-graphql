c93235db3eef315ec8aded701e4819fccaa8f7c0
Abiel <abiel@Abiels-Mac-mini.local>
2023-08-10 13:32:38 -0400

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant='h1'></Typography>
+        <Typography variant='h1'>This is the first test product that I have created on the AWS server</Typography>
       </header>
     </div>
   );
