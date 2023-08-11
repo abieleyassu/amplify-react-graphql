6b7c0b379deeaee707f24994079c6b391b0579cd
Abiel <abiel@Abiels-Mac-mini.local>
2023-08-11 00:57:44 -0400

    updated home page ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Heading } from '@aws-amplify/ui-react';
+import {Flex, Heading, ToggleButton} from '@aws-amplify/ui-react';
+import {borderRadius, width} from '@mui/system';
+import {Button, Typography} from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Heading level={2}>
-        Heading Text
-      </Heading>
-      </header>
-    </div>
-  );
+
+    const [isPressed, setIsPressed] = React.useState(false);
+
+    return (
+        <Flex style={
+            {
+                height: '100%',
+                width: '100%',
+                display: 'flex',
+                justifycontent: 'center',
+                alignitems: 'center',
+                backgroundColor: '#f6f4f2',
+                borderRadius: '2em',
+                marginTop: '20%',
+                padding: "30em"
+            }
+        }>
+            <Button variant='contained'
+                isPressed={isPressed}
+                onChange={
+                    () => setIsPressed(!isPressed)
+                }
+                style={
+                    {
+                        backgroundColor: "#4b5668",
+                        width: "30em"
+                    }
+            }>
+                <Typography variant='h5'>Submit</Typography>
+            </Button>
+        </Flex>
+    );
 }
 
 export default App;
