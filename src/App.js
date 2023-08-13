ba583c4c4a92222a07be4e994f4e6f896579097f
Abiel Eyassu <abieleyassu@gmail.com>
2023-08-13 11:23:00 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Heading } from "@aws-amplify/ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Heading level={2}>Heading Text</Heading>
+      </header>
+    </div>
+  );
+}
+
+export default App;
